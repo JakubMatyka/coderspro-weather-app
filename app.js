838e505ab15843334ae9312060224040469aa565
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-07 01:09:54 +0200

    Define angular application module

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+// Declare a variable that goes into the global namespace
+// Use directive to tell what app i`m going to use
+
 'use strict';
-require('bootstrap')
+
+// Define module
+var weatherApp = angular.module('weatherApp', []);
