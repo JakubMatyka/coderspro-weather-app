a081237ad527b273eac0033559455e7288082a88
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-07 01:34:35 +0200

    Set routing

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-// Declare a variable that goes into the global namespace
-// Use directive to tell what app i`m going to use
+// Home page with input where we can place city name so we can get some forecast for next days
+// Two pages using routing
 
 'use strict';
 
-// Define module
-var weatherApp = angular.module('weatherApp', []);
+// Add dependency of angular route and angular resource so we can use them
+// The ngRoute module provides routing and deeplinking services and directives for AngularJS apps.
+// The ngResource module provides interaction support with RESTful services via the $resource service.
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// Routes
+weatherApp.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/home.html',
+      controller: 'homeController'
+    })
+    .when('/forecast', {
+      templateUrl: '/views/forecast.html',
+      controller: 'forecastController'
+    })
+}]);
+
+// Controllers
+weatherApp.controller('homeController', ['$scope', function ($scope) {
+  console.log('home controller');
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function ($scope) {
+  console.log('forecast controller');
+}]);
