3a2afdcc149e8e402b8d570b12046f9f867a9a29
Kuba Matyka <kmatyka@ntrc.eu>
2017-04-07 01:39:24 +0200

    Add an input for typing city name

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-// Home page with input where we can place city name so we can get some forecast for next days
-// Two pages using routing
+// Input for city
 
 'use strict';
 
-// Add dependency of angular route and angular resource so we can use them
-// The ngRoute module provides routing and deeplinking services and directives for AngularJS apps.
-// The ngResource module provides interaction support with RESTful services via the $resource service.
+// Module
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // Routes
